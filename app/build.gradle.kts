8b4245c82f672911f2a62c24d3141212a0febadb
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-06-21 00:11:55 +0300

    update Target SDK to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.play.services.mlkit.document.scanner)
     // Unit Test
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
